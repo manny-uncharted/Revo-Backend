72d26d3e2d620e146f958a1d7dcaee14c136b4be
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-12 23:53:32 -0600

    Feat: Added backend Structure
    - Configured NestJs and it dependencies
    - Configured all the folders
    - Made Some entities and modules
    - Configured the docker files

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+  ],
+})
+export class AppModule {}
