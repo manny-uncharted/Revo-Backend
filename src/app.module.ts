baaf7b1e759dd60df3452bdeaaf41758e8e9ce9e
Bolu Adetifa <boluadetifa@gmail.com>
2025-02-27 22:53:29 +0100

    feat(app): integrate logger into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from './config/database.config';
+import { LoggingModule } from './modules/logging/logging.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './modules/logging/interceptors/logging.interceptor';
 
 @Module({
   imports: [
@@ -15,6 +18,13 @@ import databaseConfig from './config/database.config';
         ...configService.get('database'),
       }),
     }),
+    LoggingModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerService } from './modules/logging/services/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = app.get(LoggerService);
+  logger.setContext('Bootstrap');
+
+  app.useLogger(logger);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.info(
+    `Application is running on: http://localhost:${process.env.PORT}`,
+  );
 }
 bootstrap();
