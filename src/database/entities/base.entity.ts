690276d5d2f431b62fb8670c44b5bc3000b968f8
Ryzen XP <iron4540@gmail.com>
2025-02-27 16:23:54 +0530

    Base Entity Architecture

---
+++ src/database/entities/base.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { Timestamps } from './interfaces/ timestamps.interface';
+import { SoftDelete } from './interfaces/soft-delete.interface';
+
+@Entity()
+export abstract class BaseEntity implements Timestamps, SoftDelete {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp', nullable: true })
+  deletedAt: Date | null;
+}
